6e0cd283d69f1ff88b743ca62507bd7172f3ceef
iain holmes <iain@falsevictories.com>
2022-07-16 19:51:47 +0100

    Initial Commit

---
+++ FieldWork/FieldWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FieldWorkApp.swift
+//  FieldWork
+//
+//  Created by iain on 16/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct FieldWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
